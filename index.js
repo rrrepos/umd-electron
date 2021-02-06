4b885a2cf8513dcd45fad4098f5623fa84c9bcae
RmR <connect2rmr@gmail.com>
2021-02-06 17:01:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+var fs = require('fs');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "#1e1e1e",
+    icon: "./icons/png/1024x1024.png",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.setMenu(null);
+  const _filepath = process.argv[1];
+  let arraybuffer;
+  if (_filepath) {
+    try {
+      arraybuffer = fs.readFileSync(_filepath)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  win.loadURL("http://localhost:8080/electron");
+  win.webContents.on('did-finish-load', () => {
+    if (arraybuffer) {
+      win.webContents.send("fileinfo", { "filepath": _filepath, "buffer": arraybuffer });
+    }
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+});
