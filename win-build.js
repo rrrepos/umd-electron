e0edbbbdca5423469e11cd4399669331b942f4c4
RmR <connect2rmr@gmail.com>
2021-02-08 17:13:42 +0530

    winstaller added

--- win-build.js
+++ win-build.js
@@ -1,15 +1,16 @@
 const electronInstaller = require('electron-winstaller');
 
 try {
-    await electronInstaller.createWindowsInstaller({
+    electronInstaller.createWindowsInstaller({
       appDirectory: '/tmp/build/my-app-64',
       outputDirectory: '/tmp/build/installer64',
       authors: 'RmR',
       exe: 'umd-app.1.0.0.exe',
       iconUrl: './icons/win/icon.ico',
       setupIcon: './favicon.ico'
+    }).then(res => {
+        console.log('It worked!');
     });
-    console.log('It worked!');
   } catch (e) {
     console.log(`No dice: ${e.message}`);
   }
